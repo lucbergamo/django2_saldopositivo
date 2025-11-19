c548fa89c4f9ddaa2061a72b78b47f11d8830009
Luciano Macedo Bergamo <luciano.bergamo@ultrapar.com.br>
2025-11-19 09:10:24 -0300

    ajustes de rota

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Exemplo de rota
+    path('', views.index, name='index'),
+    path('gastos_var', views.gastos_var, name='gastos_var'), 
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def gastos_var(request):
+    return render(request, 'gastos_var.html')
