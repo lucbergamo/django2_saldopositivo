3e059dd5e03ef0b9a75cd6bdeb5c7452affa6723
lucbergamo <59293195+lucbergamo@users.noreply.github.com>
2025-11-20 22:45:42 -0300

    arquivos estaticos adicionados

--- saldo_positivo/settings.py
+++ saldo_positivo/settings.py
@@ -119,7 +119,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "core/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # ex.: /meuprojeto/static/
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
