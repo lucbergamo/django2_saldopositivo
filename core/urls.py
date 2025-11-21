1f46788445013c9e7e482c4e1831fdb64421c92b
Luciano Macedo Bergamo <luciano.bergamo@ultrapar.com.br>
2025-11-21 09:48:40 -0300

    Inclusão tabela de gastos variáveis

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('gastos_var', views.gastos_var, name='gastos_var'), 
+    path('gastos_var_lista', views.gastos_var_lista, name='gastos_var_lista')
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse , request
 from django.shortcuts import render, redirect
 from .forms import GastoVariavelForm
+from .models import GastoVariavel
 
 def index(request):
     return render(request, 'index.html')
+
+def gastos_var_lista(request):
+    gastos_variaveis = GastoVariavel.objects.all().order_by('data_gasto')
+    context = {"gastos_var_todos": gastos_variaveis}
+    return render(request, 'gastos_var lista.html', context)
 
 def gastos_var(request):
     if request.method == 'POST':
