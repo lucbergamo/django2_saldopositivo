370a719ac80c482120b8ec89df5a6edd5cbeb787
lucbergamo <59293195+lucbergamo@users.noreply.github.com>
2025-11-23 10:59:56 -0300

    Edição de gastos

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('gastos_var', views.gastos_var, name='gastos_var'), 
-    path('gastos_var_lista', views.gastos_var_lista, name='gastos_var_lista')
+    path('gastos_var_lista', views.gastos_var_lista, name='gastos_var_lista'),
+    path('edit_gastos_var/<int:pk>', views.edit_gastos_var, name='edit_gastos_var')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse , request
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import GastoVariavelForm
 from .models import GastoVariavel
 
@@ -20,3 +20,14 @@ def gastos_var(request):
     else:
         form = GastoVariavelForm()
     return render(request, 'gastos_var.html', {'form':form})
+
+def edit_gastos_var(request, pk):
+    editGasto = get_object_or_404(GastoVariavel, pk=pk)
+    if request.method == 'POST':
+        form = GastoVariavelForm(request.POST, instance=editGasto)
+        if form.is_valid():
+            form.save()
+            return redirect('gastos_var_lista')
+    else:
+        form = GastoVariavelForm(instance=editGasto)
+    return render(request, 'edit_gastos_var.html', {'form':form, 'edit_Gasto': editGasto})          
