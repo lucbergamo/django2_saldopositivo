9639aff1ca3e0e8ab5230beb0436f94e48e913c8
Luciano Macedo Bergamo <luciano.bergamo@ultrapar.com.br>
2025-12-12 09:15:35 -0300

    campo data

--- core/forms.py
+++ core/forms.py
@@ -5,16 +5,18 @@ class GastoVariavelForm(forms.ModelForm):
     class Meta:
         model = GastoVariavel
         fields = ['titulo', 'data_gasto', 'valor', 'tipo']
+        
 
-        # 1. Definir o widget para o campo data_gasto
         widgets = {
-            'data_gasto': forms.DateInput(attrs={
-                # Essa classe 'datepicker' será usada pelo JavaScript
-                'class': 'form-control datepicker', 
-                'type': 'text' # Importante: Define como 'text' para que o navegador não use o datepicker nativo
-            })
+            'data_gasto': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control',  # integra com Bootstrap
+                    # 'placeholder': 'Selecione a data', # opcional
+                },
+                format='%Y-%m-%d'  # formato aceito pelo input date
+            )
         }
-
 
 class RegistroGastoFixoForm(forms.ModelForm):
     class Meta:
@@ -31,6 +33,19 @@ class ReceitaFixaForm(forms.ModelForm):
     class Meta:
         model = ReceitaFixa
         fields = ['titulo', 'dataRecebimento','receitaEsperada', 'bancoRecebedor']
+        
+        widgets = {
+            'dataRecebimento': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control',  # integra com Bootstrap
+                    # 'placeholder': 'Selecione a data', # opcional
+                },
+                format='%Y-%m-%d'  # formato aceito pelo input date
+            )
+        }
+                       
+
 
 class RegistroReceitaFixaForm(forms.ModelForm):
     class Meta:
--- core/models.py
+++ core/models.py
@@ -20,17 +20,17 @@ class GastoVariavel(models.Model):
 class GastoFixo(models.Model):
     titulo = models.CharField(max_length=200)
     criado = models.DateField(auto_created=True)
-    dia_pagamento = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)],
+    dia_pagamento = models.IntegerField(verbose_name='Dia do Pagamento', validators=[MinValueValidator(1), MaxValueValidator(31)],
         help_text="Dia do mês em que normalmente se paga este gasto (1 a 31).")
     
-    bancoPagador = models.CharField(max_length=500)
+    bancoPagador = models.CharField(max_length=500, verbose_name='Banco que recebe o pagamento')
     TIPOS = [
         ('Débito Automatico', 'Débito Automatico'),
         ('Pix', 'Pix'),
         ('Débito em Conta', 'Débito em Conta'),
         ('Cartão de Crédito', 'Cartão de Crédito'),
     ]
-    formaPagamento = models.CharField(max_length=30, choices=TIPOS, blank=True, null=True)
+    formaPagamento = models.CharField(max_length=30, choices=TIPOS, blank=True, null=True, verbose_name='Forma de Pagamento')
 
     def __str__(self):
         return f"{self.titulo}" 
@@ -40,21 +40,22 @@ class RegistroGastoFixo(models.Model):
     gasto_fixo = models.ForeignKey(
         GastoFixo,
         on_delete=models.PROTECT,       # evita apagar um gasto enquanto há registros
-        related_name='registros'
+        related_name='registros', 
+        verbose_name='Gasto Fixo'
     )
     criado = models.DateField(auto_now_add=True)
-    dataPagamento = models.DateField()
-    valorPago = models.DecimalField(max_digits=10, decimal_places=2)
+    dataPagamento = models.DateField(verbose_name='Data do Programada do pagamento')
+    valorPago = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor Pago')
 
     def __str__(self):
         return f"{self.titulo} - {self.valorPago}"
     
 class ReceitaFixa(models.Model):
-    titulo = models.CharField(max_length=80)
-    dataRecebimento = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)],
+    titulo = models.CharField(max_length=80, verbose_name='Título')
+    dataRecebimento = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)], verbose_name='Data de Recebimento',
         help_text="Dia do mês em que normalmente se paga este gasto (1 a 31).")
-    receitaEsperada = models.DecimalField(max_digits=10, decimal_places=2)
-    bancoRecebedor = models.CharField(max_length=50)
+    receitaEsperada = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Receita Esperada')
+    bancoRecebedor = models.CharField(max_length=50, verbose_name='Banco Recebedor')
  #   criado = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -64,10 +65,10 @@ class RegistroReceitaFixa(models.Model):
     receita_fixa = models.ForeignKey(
         ReceitaFixa,
         on_delete=models.PROTECT,       # evita apagar um gasto enquanto há registros
-        related_name='registros'
+        related_name='registros', verbose_name='Receita Fixa'
     )
-    dataRecebimento = models.DateField()
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    dataRecebimento = models.DateField(verbose_name='Data Recebimento')
+    valor = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor')
     
     def __str__(self):
         return f"{self.titulo} - {self.valor}"
